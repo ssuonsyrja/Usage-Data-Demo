f909efbe8c711c7e1105e71cd2bff18d27730993
ssuonsyrja <ssuonsyrja@gmail.com>
2016-09-28 12:09:52 +0300

    jqxgrid example added

--- lock/js/app.js
+++ lock/js/app.js
@@ -4,10 +4,51 @@
 // except 'app' ones,
 requirejs.config({
     "baseUrl": "js/lib",
+    "shim" : {
+        "bootstrap" : { "deps" :['jquery'] },
+        "jqxcore": { "export": "$", "deps": ['jquery'] },
+        "jqxdata": { "export": "$", "deps": ['jquery', "jqxcore"] },
+        "jqxbuttons": { "export": "$", "deps": ['jquery', "jqxcore"] },
+        "jqxscrollbar": { "export": "$", "deps": ['jquery', "jqxcore"] },
+        "jqxlistbox": { "export": "$", "deps": ['jquery', "jqxcore"] },
+        "jqxdropdownlist": { "export": "$", "deps": ['jquery', "jqxcore"] },
+        "jqxmenu": { "export": "$", "deps": ['jquery', "jqxcore"] },
+        "jqxgrid": { "export": "$", "deps": ['jquery', "jqxcore"] },
+        "jqxgrid.filter": { "export": "$", "deps": ['jquery', "jqxcore"] },
+        "jqxgrid.sort": { "export": "$", "deps": ['jquery', "jqxcore"] },
+        "jqxgrid.selection": { "export": "$", "deps": ['jquery', "jqxcore"] },
+        "jqxpanel": { "export": "$", "deps": ['jquery', "jqxcore"] },
+        "globalize": { "export": "$", "deps": ['jquery', "jqxcore"] },
+        "jqxcalendar": { "export": "$", "deps": ['jquery', "jqxcore"] },
+        "jqxdatetimeinput": { "export": "$", "deps": ['jquery', "jqxcore"] },
+        "jqxcheckbox": { "export": "$", "deps": ['jquery', "jqxcore"] },
+        "demo": { "export": "$", "deps": ['jquery', "jqxcore"] },
+        "generatedata": { "export": "$", "deps": ['jquery', "jqxcore"] }
+    },
     "paths": {
       "app": "../app",
-      "jquery": "https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min"
+      "jquery" : "jquery-1.11.1.min", //"https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min",
+      "bootstrap" :  "http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min",
+      "jqxcore" : "../../../../CollectorTests/jqwidgets/jqxcore",
+      "jqxdata" : "../../../../CollectorTests/jqwidgets/jqxdata",
+      "jqxbuttons" : "../../../../CollectorTests/jqwidgets/jqxbuttons",
+      "jqxscrollbar" : "../../../../CollectorTests/jqwidgets/jqxscrollbar",
+      "jqxlistbox" : "../../../../CollectorTests/jqwidgets/jqxlistbox",
+      "jqxdropdownlist" : "../../../../CollectorTests/jqwidgets/jqxdropdownlist",
+      "jqxmenu" : "../../../../CollectorTests/jqwidgets/jqxmenu",
+      "jqxgrid" : "../../../../CollectorTests/jqwidgets/jqxgrid",
+      "jqxgrid.filter" : "../../../../CollectorTests/jqwidgets/jqxgrid.filter",
+      "jqxgrid.sort" : "../../../../CollectorTests/jqwidgets/jqxgrid.sort",
+      "jqxgrid.selection" : "../../../../CollectorTests/jqwidgets/jqxgrid.selection",
+      "jqxpanel" : "../../../../CollectorTests/jqwidgets/jqxpanel",
+      "globalize" : "../../../../CollectorTests/jqwidgets/globalization/globalize",
+      "jqxcalendar" : "../../../../CollectorTests/jqwidgets/jqxcalendar",
+      "jqxdatetimeinput" : "../../../../CollectorTests/jqwidgets/jqxdatetimeinput",
+      "jqxcheckbox" : "../../../../CollectorTests/jqwidgets/jqxcheckbox",
+      "demo" : "../../../../CollectorTests/scripts/demos",
+      "generatedata" : "../../../../CollectorTests/demos/jqxgrid/generatedata"
     }
+
 });
 
 // Load the main app module to start the app
--- lock/js/app/main.js
+++ lock/js/app/main.js
@@ -1,4 +1,8 @@
-define(["jquery"], function($) {
+define(["jquery", 'bootstrap', "jqxcore", "jqxdata", "jqxbuttons",
+  "jqxscrollbar", "jqxlistbox", "jqxdropdownlist", "jqxmenu", "jqxgrid",
+  "jqxgrid.filter", "jqxgrid.sort", "jqxgrid.selection", "jqxpanel",
+  "globalize", "jqxcalendar", "jqxdatetimeinput", "jqxcheckbox", "demo",
+  "generatedata"], function($) {
 
   $(function() {
 
@@ -25,6 +29,7 @@ define(["jquery"], function($) {
       require(['../../Usage-Data-Collector/UDC/app/scripts/udc.js'], function (UDC) {
           var udc = new UDC;
           udc.startTracking("body", "click", "input");
+          udc.startTracking("body", "mouseenter", "#paraId")
       });
 
 
@@ -54,6 +59,100 @@ define(["jquery"], function($) {
               release();
           }
       );
+
+      var para = document.createElement("p");
+      var node = document.createTextNode("This is new.");
+      para.setAttribute("id", "paraId");
+      para.appendChild(node);
+
+      var element = document.getElementById("body1");
+      element.appendChild(para);
+
+      var data = generatedata(500);
+      var source =
+      {
+          localdata: data,
+          datafields:
+          [
+              { name: 'firstname', type: 'string' },
+              { name: 'lastname', type: 'string' },
+              { name: 'productname', type: 'string' },
+              { name: 'date', type: 'date' },
+              { name: 'quantity', type: 'number' },
+              { name: 'price', type: 'number' }
+          ],
+          datatype: "array"
+      };
+
+      var addfilter = function () {
+          var filtergroup = new $.jqx.filter();
+
+          var filter_or_operator = 1;
+          var filtervalue = 'Beate';
+          var filtercondition = 'contains';
+          var filter1 = filtergroup.createfilter('stringfilter', filtervalue, filtercondition);
+
+          filtervalue = 'Andrew';
+          filtercondition = 'starts_with';
+          var filter2 = filtergroup.createfilter('stringfilter', filtervalue, filtercondition);
+
+          filtergroup.addfilter(filter_or_operator, filter1);
+          filtergroup.addfilter(filter_or_operator, filter2);
+          // add the filters.
+          $("#jqxgrid").jqxGrid('addfilter', 'firstname', filtergroup);
+          // apply the filters.
+          $("#jqxgrid").jqxGrid('applyfilters');
+      }
+      var adapter = new $.jqx.dataAdapter(source);
+
+      $("#jqxgrid").jqxGrid(
+      {
+          width: 850,
+          source: adapter,
+          filterable: true,
+          sortable: true,
+          ready: function () {
+              addfilter();
+          },
+          autoshowfiltericon: true,
+          columns: [
+            { text: 'First Name', datafield: 'firstname', width: 160 },
+            { text: 'Last Name', datafield: 'lastname', width: 160 },
+            { text: 'Product', datafield: 'productname', width: 170 },
+            { text: 'Order Date', datafield: 'date', filtertype: 'date', width: 160, cellsformat: 'dd-MMMM-yyyy' },
+            { text: 'Quantity', datafield: 'quantity', width: 80, cellsalign: 'right' },
+            { text: 'Unit Price', datafield: 'price', cellsalign: 'right', cellsformat: 'c2' }
+          ]
+      });
+
+      $('#events').jqxPanel({ width: 300, height: 80});
+
+      $("#jqxgrid").on("filter", function (event) {
+          $("#events").jqxPanel('clearcontent');
+          var filterinfo = $("#jqxgrid").jqxGrid('getfilterinformation');
+
+          var eventData = "Triggered 'filter' event";
+          for (i = 0; i < filterinfo.length; i++) {
+              var eventData = "Filter Column: " + filterinfo[i].filtercolumntext;
+              $('#events').jqxPanel('prepend', '<div style="margin-top: 5px;">' + eventData + '</div>');
+          }
+      });
+
+      $('#clearfilteringbutton').jqxButton({ height: 25});
+      $('#filterbackground').jqxCheckBox({ checked: true, height: 25});
+      $('#filtericons').jqxCheckBox({ checked: false, height: 25});
+      // clear the filtering.
+      $('#clearfilteringbutton').click(function () {
+          $("#jqxgrid").jqxGrid('clearfilters');
+      });
+      // show/hide filter background
+      $('#filterbackground').on('change', function (event) {
+          $("#jqxgrid").jqxGrid({ showfiltercolumnbackground: event.args.checked });
+      });
+      // show/hide filter icons
+      $('#filtericons').on('change', function (event) {
+          $("#jqxgrid").jqxGrid({ autoshowfiltericon: !event.args.checked });
+      });
     });
 
     function lock() {
